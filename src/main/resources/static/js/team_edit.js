54141838ebc1bebf02091067772b66a0e81ea02f
OkadaKeisuke <sonasoku1354@gmail.com>
2021-02-08 04:09:03 +0900

    一覧ページのデザインを修正。サモナーテーブル以外修正完了。

--- src/main/resources/static/js/team_edit.js
+++ src/main/resources/static/js/team_edit.js
@@ -1,9 +1,43 @@
 
+/*
+ * 更新時確認メソッド
+ */
 function alertEdit() {
+
+	// 更新の確認ポップアップ
 	if (window.confirm("この内容で更新しますか？")) {
+
+		// 更新前処理
+		return prepareEdit();
+
 		return true;
 	} else {
 		return false;
 	}
+}
 
+/*
+ * 更新前処理
+ */
+function prepareEdit() {
+	// チーム名の前後空白を削除
+	var teamName = document.getElementsByClassName("team_name_input");
+	teamName[0].value = teamName[0].value.trim();
+
+	if (teamName[0].value === "") {
+		window.alert("チーム名を入力してください。");
+		return false;
+	}
+
+
+	// サモナー名の前後空白を削除
+	var summonerNameCollections = document.getElementsByClassName("summoner_name_input");
+	var summonerNameArray = Array.from(summonerNameCollections);
+	summonerNameArray.forEach(summonerName => {
+		summonerName.value = summonerName.value.trim();
+	});
+
+	// 紹介文の前後空白を削除
+	var teamIntroduction = document.getElementsByClassName("team_intro_textarea");
+	teamIntroduction[0].value = teamIntroduction[0].value.trim();
 }
