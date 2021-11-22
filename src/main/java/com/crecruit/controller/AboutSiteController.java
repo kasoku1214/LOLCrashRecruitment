8afdb1a44ecb63303d29e6abef20f35171244fc9
kasoku1214 <sonasoku1354@gmail.com>
2021-11-23 03:57:01 +0900

    端末間テスト終了。元に戻す

--- src/main/java/com/crecruit/controller/AboutSiteController.java
+++ src/main/java/com/crecruit/controller/AboutSiteController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-// 端末間gitテスト
 
 @Controller
 public class AboutSiteController {
